d95b740951fa8e0a18412a6442875a57d22a205c
Rupam Bhakta <rupambhakta2020@gmail.com>
2026-01-04 12:18:49 +0530

    installed @clerk/clerk-expo

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <Slot />
+    </ClerkProvider>
+  );
 }
